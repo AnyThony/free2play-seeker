0ee458fb4a2a85a9ca2967e17ce9fa803ad78571
AnyThony <28424838+AnyThony@users.noreply.github.com>
2020-12-19 05:04:02 -0500

    add: slider functionality

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,11 +1,41 @@
+import React from "react";
 import "../styles/ImageSlider.css";
 
 function ImageSlider(props) {
-    
+    const [index, setIndex] = React.useState(0);
+    const { images } = props;
+
+    function slideControl(direction) {
+        const lastIndex = images.length - 1
+        if (direction) { // slide left
+            if (index == 0)
+                setIndex(lastIndex);
+            else
+                setIndex(index - 1);
+        }
+        else { // slide right
+            if (index == lastIndex)
+                setIndex(0);
+            else
+                setIndex(index + 1);
+        }
+    }
+
     return (
         <div className="image-slider">
-            <img src={props.images[0]}/>
-
+            <img src={images[index]} onClick={() => slideControl(true)}/>
+            <div
+                className="control slide-left"
+                onClick={() => slideControl(true)}
+            >
+                <span>&lt;</span>
+            </div>
+            <div
+                className="control slide-right"
+                onClick={() => slideControl(false)}
+            >
+                <span>&gt;</span>
+            </div>
         </div>
     );
 }
