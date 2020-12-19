1f0a9cabcf8238d42f4640103f3b0ca40bf80dd1
AnyThony <28424838+AnyThony@users.noreply.github.com>
2020-12-19 02:06:39 -0500

    load data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,43 @@
-
-import './Home.css';
+import React from "react"
+import '../styles/Home.css';
 import Banner from "../components/Banner";
-import NavBar from "../components/NavBar"
+import NavBar from "../components/NavBar";
+import ContentModal from "../components/ContentModal";
+import CategorySlider from "../components/CategorySlider";
+import { getCategories } from "../services"
+
+const EMPTY_CATEGORIES = { 
+    shooter: [],
+    mmorpg: [],
+    pvp: [],
+    mmofps: []
+}
 
 function Home() {
-  return (
-    <div className="Home">
-        <Banner/>
-        <NavBar/>
-    </div>
-  );
+    const [categoryData, setCategoryData] = React.useState(EMPTY_CATEGORIES)
+
+    React.useEffect(() => {
+        async function fetchCategory() {
+            const categories = await getCategories();
+            console.log(categories)
+            setCategoryData(categories);
+        }
+        fetchCategory();
+    }, [])
+
+    return (
+        <div className="Home">
+            <Banner />
+            <NavBar />
+            <div className="content-body">
+                <CategorySlider label={"Shooter"} items={categoryData.shooter}/>
+                <CategorySlider label={"MMORPG"} items={categoryData.mmorpg}/>
+                <CategorySlider label={"Player vs Player"} items={categoryData.pvp}/>
+                <CategorySlider label={"MMOFPS"} items={categoryData.mmofps}/>
+            </div>
+            <ContentModal/>
+        </div>
+    );
 }
 
 export default Home;
