430672dee7fc8de03117011ac801b68f9d63841f
AnyThony <28424838+AnyThony@users.noreply.github.com>
2020-12-19 16:45:49 -0500

    uri fetch fix

--- src/services.js
+++ src/services.js
@@ -2,15 +2,14 @@ import config from "./config.json";
 import GamesByCategory from "./mock/GamesByCategory.json"
 import GameById from "./mock/GamesById.json"
 
-const CATEGORY_NAMES = ["shooter", "mmorpg", "mmofps", "pvp"];
-const BASE_URI = "https://www.freetogame.com/api/games";
+const CATEGORY_NAMES = ["shooter", "mmorpg", "mmofps", "pvp"]
 
 async function getCategories() {
     if (config.useMock) return GamesByCategory;
 
     let result = {}
     for (const name of CATEGORY_NAMES){
-        const uri = `${BASE_URI}?category=${name}`
+        const uri = `https://www.freetogame.com/api/games?category=${name}`
         const response = await fetch(uri);
         const responseJSON = await response.json()
         result[name] = responseJSON.slice(0,10);
@@ -21,7 +20,7 @@ async function getCategories() {
 async function getGameById(id){
     if (config.useMock) return GameById[id.toString()];
 
-    const uri = `${BASE_URI}?id=${id}`;
+    const uri = `https://www.freetogame.com/api/game?id=${id}`;
     const response = await fetch(uri);
     return await response.json()
 }
