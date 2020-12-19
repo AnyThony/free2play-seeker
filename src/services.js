526d47cada4b87d64bac61ad780085235b9c5e4e
AnyThony <28424838+AnyThony@users.noreply.github.com>
2020-12-19 04:06:39 -0500

    add: mock support

--- src/services.js
+++ src/services.js
@@ -1,13 +1,27 @@
+import config from "./config.json";
+import GamesByCategory from "./mock/GamesByCategory.json"
+import GameById from "./mock/GamesById.json"
+
 const CATEGORY_NAMES = ["shooter", "mmorpg", "mmofps", "pvp"]
 
 async function getCategories() {
+    if (config.useMock) return GamesByCategory;
+
     let result = {}
     for (const name of CATEGORY_NAMES){
         const response = await fetch(`https://www.freetogame.com/api/games?category=${name}`);
         const responseJSON = await response.json()
         result[name] = responseJSON.slice(0,10);
     }
+    console.log(JSON.stringify(result))
     return result;
 }
 
-export { getCategories }
+async function getGameById(id){
+    if (config.useMock) return GameById[id.toString()];
+
+    const response = await fetch(`https://www.freetogame.com/api/game?id=${id}`);
+    return await response.json()
+}
+
+export { getCategories, getGameById }
