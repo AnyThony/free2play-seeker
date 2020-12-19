9ac0f6afb23e095ac582f2b4c4c394eb0d9b1ac1
AnyThony <28424838+AnyThony@users.noreply.github.com>
2020-12-18 21:29:20 -0500

    add: Home Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+
+import './Banner.css';
+
+function Banner() {
+    return (
+        <div className="banner">
+            <div className="banner-bg">
+                <img src="https://wallpapercave.com/wp/wc1765288.jpg" alt="..." />
+                <div className="bg-gradient"></div>
+            </div>
+            <div className="banner-content">
+                <h2 className="banner-title">
+                    Free To Play Games
+                </h2>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+
+import './Home.css';
+import Banner from "../components/Banner";
+import NavBar from "../components/NavBar"
+
+function Home() {
+  return (
+    <div className="Home">
+        <Banner/>
+        <NavBar/>
+    </div>
+  );
+}
+
+export default Home;
